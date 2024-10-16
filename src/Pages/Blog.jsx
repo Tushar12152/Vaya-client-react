2f91ddde1b432dcffa8bd8c609f295fa472529bc
Tushar <tusharimran470@gmail.com>
2024-10-16 13:25:16 +0600

    blog adding

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,7 +1,13 @@
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div>
+
+        <div className="h-72 bg-slate-500">
+            <h1 className="text-center font-bold pt-24 text-white text-4xl">BLOG</h1>
+        </div>
+
+    </div>
   )
 }
 
